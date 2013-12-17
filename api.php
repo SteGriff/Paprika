441ba62677b6cfc2b1db4a1cf0620765d75a0fcf
Stephen Griffiths <github@stegriff.co.uk>
2013-12-17 14:23:36 +0000

    Ready for paprika.me.uk

--- api.php
+++ api.php
@@ -6,6 +6,7 @@ require 'ext.php';
 require 'grammar.php';
 
 $DEBUG = from_request('debug');
+$ERROR = null;
 
 //Plain text content-type? 
 if (from_request('text') || $DEBUG){
@@ -16,49 +17,136 @@ if (from_request('text') || $DEBUG){
 $q = from_request('q');
 $result = parse($q);
 
+function setError($text){
+	global $ERROR;
+	$ERROR = "\n<p>*$text</p>\n";
+	return $ERROR;
+}
+
 function randomFrom($range){
 	return $range[array_rand($range)];
 }
 
 function parse($v){
 	global $DEBUG;
-	
+	global $ERROR;
+
 	//While string contains open square
 	$open = strpos($v, '[');
+	
+	// If we do a loop with no useful operation, nops increases
+	// If nops is 2, we exit with error.
+	$lastV = '';
+	$nops = 0;
+	
 	while ($open !== false){
+	
+		//Infinite loop check
+		if ($v == $lastV){
+			$nops++;
+			if ($nops == 2){
+				return setError("Stuck in a loop. Please email me the formula you entered.");
+			}
+		}
+		
+		$lastV = $v;
+		
+		//OK Go
+		//Reset command flag
+		$flag = null;
+		
 		if ($DEBUG) { echo "(v: '$v')\n"; }
+		
+		//Check whether this bracket is mismatched
 		$close = strpos($v, ']');
 		if ($close === false){
-			return "\n*Mismatched bracket from char $open\n";
+			return setError("Mismatched bracket from char $open");
 		}
 		
-		//The bracketed expression, like [sport]
-		$expression = substr($v, $open, $close + 1 - $open);
+		//Get the bracketed expression, like [sport] or [U sport]
+		//$target will be replaced by $resolution
+		$target = substr($v, $open, $close + 1 - $open);
+		//$expression will be stripped of commands, to leave a grammar key.
+		$expression = $target;
 
-		//Handle hidden early calls
-		if ($expression[1] == "!"){
-			$v = str_replace($expression, '', $v);
-			$expression = str_replace('!', '', $expression);
+		//A bool for whether the $target needs to be transformed with expression.
+		$retarget = null;
+		
+		//Pre-treatment the formula or mark for posttreatment using $flag
+		if ($expression[2] == ' '){
+			//This means the expression starts with a symbol then a space, like [U animal]
+			switch ($expression[1]){
+				case 'U':
+					//Uppercase-ify
+					$flag = $expression[1];
+					break;
+				case 'L':
+					//Lowercase-ify
+					$flag = $expression[1];
+					break;
+				case 'H':
+					//Hide this tag but replace other instances (for early calls)
+					$v = str_replace($expression, '', $v);
+					//Update target after the 'H ' is removed
+					// because we want to replace [sport] not [H sport]
+					$retarget = true;
+					break;
+				case '?':
+					//Flip a coin. If tails, render this expression blank (later)
+					if (mt_rand(0,1) === 0){
+						$flag = $expression[1];
+					}
+					break;
+			}
+			//Now remove the command and space so that the expression can be recognised in grammar
+			$expression = $expression[0] . substr($expression, 3);
+			if ($retarget) {$target = $expression;}
 		}
 		
+		//$resolution is a plain string from grammar, like 'football'
 		$resolution = resolveBracket($expression);
 		if ($DEBUG) { echo "(Resolution: '$resolution')\n";}
 				
-		$v = str_replace($expression, $resolution, $v);
+		//Handle any processing flags we set earlier
+		switch ($flag){
+			case 'U':
+				$resolution = strtoupper($resolution);
+				break;
+			case 'L':
+				$resolution = strtolower($resolution);
+				break;
+			case '?':
+				$resolution = '';
+				break;
+		}
+		
+		//Clean up double spaces - could make this a while loop
+		$v = str_replace('  ',' ',$v);
+		
+		//THIS IS IT FOLKS
+		//Replace all instances of [target] with its resolution
+		$v = str_replace($target, $resolution, $v);
 		if ($DEBUG) { echo "(New v: '$v')\n";}
+		
+		//Break out if there has been an error in this stage
+		if ($ERROR) { return $ERROR; }
+		
 		//Get next open for while loop
 		$open = strpos($v, '[');
 	}
 	
+	// We've now replaced all [expression]s
+	
+	// Replace all the a/an words which have been marked with Chr(254)-Chr(255)
+	// TODO: Let's do this a better way sometime in the future
 	$open = strpos($v, 'þ');
 	while ($open !== false){
 		$close = strpos($v, 'ÿ');
 		if ($close === false){
-			return "\n*Bad a/an matching. Developer error.";
+			return setError("Bad a/an matching. Developer error.");
 		}
 		$expression = substr($v, $open, $close + 1 - $open);
 		$nextChar = trim(substr($v, $close + 1))[0];
-		//die( "\n~~$nextChar~~");
 		$resolution = isVowel($nextChar) ? 'an' : 'a';
 		$v = str_replace($expression, $resolution, $v);
 		$open = strpos($v, 'þ');
@@ -67,39 +155,42 @@ function parse($v){
 	return $v;
 }
 
-function resolveBracket($v){
+// Takes a bracketed expression like '[sport]' and resolves it into
+// a grammar entry (like 'football') or an error.
+// $bw bracketed word
+function resolveBracket($bw){
 
 	global $grammar;
 	
 	//Remove square brackets (first and last char)
-	$v = substr($v, 1, strlen($v) - 2);
+	$bw = substr($bw, 1, strlen($bw) - 2);
 
-	if (strpos($v, '[') !== false || strpos($v, ']') !== false){
-		return "\n*Nested brackets; make an early call - see docs\n";
+	if (strpos($bw, '[') !== false || strpos($bw, ']') !== false){
+		return setError("Nested brackets; make an early call - see docs");
 	}
 	
 	//Handle a label inside the tag like [sport#1]
-	$labelPosition = strpos($v, '#');
+	$labelPosition = strpos($bw, '#');
 	if ($labelPosition !== false){
-		$v = substr($v, 0, $labelPosition);
+		$bw = substr($bw, 0, $labelPosition);
 	}
 	
 	//If its type exists in grammar, pick a random member
-	if ($grammar[$v]){
-		return randomFrom($grammar[$v]);
+	if ($grammar[$bw]){
+		return randomFrom($grammar[$bw]);
 	}
-	elseif ($v == 'a' || $v == 'an'){
+	elseif ($bw == 'a' || $bw == 'an'){
 		//Deal with these on a seperate iteration
-		return "þ{$v}ÿ";
+		return "þ{$bw}ÿ";
 	}
 	else{
-		return "\n*Unknown: '$v'\n";
+		return setError("Unknown: '$bw'");
 	}
 }
 
 function isVowel($c){
 	//Can find character c in vowel array?
-	return strpos('aeiouy', $c) !== false;
+	return strpos('aeiou', $c) !== false;
 }
 
 ?>
--- readme.md
+++ readme.md
@@ -1,10 +1,88 @@
-#Flavour generator
-Candidate name: "Paprika"
+#Paprika
 
-Generates flavour text and natural language from human-readable grammar configuration files
+Generates flavour text and natural language from human-readable grammar configuration files.
+<http://paprika.me.uk>
 
-## Status
-Not ready for prime time yet
+## Future applications
 
------
-@SteGriff
+ + Tweetbot
+ + Procedural generation of NPC monologue
+ + Descriptions or flavour text of entities or dungeons
+
+## How to test
+
+To play with Paprika, go to <http://paprika.me.uk>. Learn about the Formula language below. Click Explore on Paprika.me.uk/use to view the grammar.
+
+## Paprika Formula Language
+
+To get output, you send commands in Paprika Formula Language. Here is a tutorial/reference:
+
+String literals are returned in output, identical to their input:
+	Hello
+Produces "Hello"
+
+Expressions in square brackets pick a random definition from that category, as defined in grammar files:
+	[animal]
+May produces "dog" or "cat", etc.
+
+The tags `[a]` and `[an]` are magical. They wait until everthing else has been populated, and then pick correctly between
+'a' and 'an', based on the first letter of the following word. They are both supplied so that the Formula can read naturally:
+	[an] [animal]
+May produce "a dog" or "an otter".
+N.b. Currently 'aeiou' is considered to be the set of vowels which require 'an'.
+
+If you use identical tags more than once, they will be populated with the same thing (think of it as a global find+replace):
+	My [animal] is great. I love my [animal].
+May produce "My cow is great. I love my cow."
+
+To generate different results, you can put labels inside tags to differentiate them, using a hash `#`:
+	My [animal#1] is great, but I also love my [animal#2]
+Be warned that this currently doesn't exclude results which have been chosen already,
+so you may get the same result in both brackets. However, the randomisation is done seperately for each one.
+
+You can inject the results of a tag into another tag. If the 'simple' tag comes first, this is easy:
+	My favourite [category] is [[category]]
+What happens here is that [category] is parsed first, so the expression becomes (for example):
+	My favourite sport is [sport]
+And then in the next pass, the `[sport]` tag is replaced by a random sport:
+	My favourite sport is badminton
+Equally, the expresion could have randomised to
+	My favourite country is Italy
+	
+If the complex part (with nested brackets) comes first in the Formula, it will fail:
+	[[category]] is the best [category]
+	 ^-- Error, no such thing as [[category]
+So you can make an 'early call' to avoid this:
+	[!category][[category]] is the best [category]
+The exclamation mark means that the result of `[!category]` will be hidden.
+It replaces all other instances, but is itself removed. Example output:
+	brown is the best colour
+	
+If the grammar allows for it, you can trigger output with strong contextual links in this way.
+The following example is not defined in my grammars, but is given for example. Assume this grammar:
+	*animal
+	dog
+	cat
+	mouse
+	
+	*did dog thing
+	barked
+	woofed
+	
+	*did cat thing
+	meowed
+	purred
+	
+	*did mouse thing
+	squeaked
+	spooked an elephant
+Then this formula:
+	My [animal] [did [animal] thing]
+May produce
+	My dog woofed
+Or
+	My mouse spooked an elephant
+	
+----------
+I hope you enjoy Paprika!
+@SteGriff - <http://stegriff.co.uk>
